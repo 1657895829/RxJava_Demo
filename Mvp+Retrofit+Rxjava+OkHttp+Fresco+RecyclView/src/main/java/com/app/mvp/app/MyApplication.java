493356660529db51188e8b8165549d3ea0e3cd8e
1657895829 <1657895829@qq.com>
2017-12-06 16:05:05 +0800

    MVP结合Retrofit+Rxjava+OkHttp单例封装请求网络数据(可添加拦截器),Fresco加载图片，RecyclerView展示数据

--- Mvp+Retrofit+Rxjava+OkHttp+Fresco+RecyclView/src/main/java/com/app/mvp/app/MyApplication.java
+++ Mvp+Retrofit+Rxjava+OkHttp+Fresco+RecyclView/src/main/java/com/app/mvp/app/MyApplication.java
@@ -1,35 +1,17 @@
 package com.app.mvp.app;
 
 import android.app.Application;
-import com.app.mvp.retrofit.GetDataInterface;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * 用于全局配置初始化异步加载类
+ * 用于全局配置初始化Fresco 图片加载
  */
 public class MyApplication extends Application {
-    //设置公共变量
-    public static GetDataInterface request;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        //1.  用于全局配置初始化Fresco 图片加载
+
         Fresco.initialize(this);
-
-        //2.  用于全局配置初始化Retrofit 网络请求  RxJava结合Retrofit
-        //构建Retrofit类，初始化参数
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.tianapi.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                // call 转化成 Observerable
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-
-        //创建网络请求接口实例
-        request = retrofit.create(GetDataInterface.class);
     }
 }
